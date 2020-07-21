bcae085d07b136141bad984e960f589bd581ee65
manish gandotra <manishgandotra74@gmail.com>
2020-07-21 14:09:43 +0530

    changes

---
+++ opt/build/repo/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { bindActionCreators } from "redux";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+function App(props) {
+  console.log(props.logoutcall);
+  let data = JSON.parse(localStorage.getItem('user'));
+  if (data === null ){
+    props.history.push('/login')
+  }else {
+
+  }
+// if ( JSON.parse(localStorage.getItem('user')) && JSON.parse(localStorage.getItem('user')).message !== "Success"){
+// }
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+
+App.propTypes = {
+  history: PropTypes.object,
+  match: PropTypes.object,
+  saveusermessage: PropTypes.string
+};
+function mapStateToProps(state) {
+  return {
+    ...state.user
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({  }, dispatch);
+}
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+    null,
+    { forwardRef: true }
+  )(App)
+);
